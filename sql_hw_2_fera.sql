11a581f58c1cfe5041b4a8b1b6820fe824519d63
Ferrum6 <91910503+Ferrum6@users.noreply.github.com>
2021-11-03 13:50:45 +0600

    Update sql_hw_2_fera.sql

--- sql_hw_2_fera.sql
+++ sql_hw_2_fera.sql
@@ -1,3 +1,8 @@
+--==Наполнить таблицу employee 70 строками.
+--==Создать таблицу employees
+--==- id. serial,  primary key,
+--==- employee_name. Varchar(50), not null
+
 create table employees
     (id serial primary key,   
     employee_name varchar(50) not null
@@ -9,6 +14,26 @@ values (default, 'Moskva Piter');
 select * from employees;
 
 --=================================
+--==Создать таблицу salary
+--==- id. Serial  primary key,
+--==- monthly_salary. Int, not null
+--==Наполнить таблицу salary 15 строками:
+--==- 1000
+--==- 1100
+--==- 1200
+--==- 1300
+--==- 1400
+--==- 1500
+--==- 1600
+--==- 1700
+--==- 1800
+--==- 1900
+--==- 2000
+--==- 2100
+--==- 2200
+--==- 2300
+--==- 2400
+--==- 2500
 
 create table salary 
 (id serial primary key, 
@@ -17,26 +42,36 @@ monthly_salary varchar (15) not null
 insert into salary (id, monthly_salary)
 values (default, 2500);
 
-select *from salary
-;
+select * from salary;
+
+
 --==================================
+--Создать таблицу employee_salary
+--- id. Serial  primary key,
+--- employee_id. Int, not null, unique
+--- salary_id. Int, not null
+--Наполнить таблицу employee_salary 40 строками:
+--- в 10 строк из 40 вставить несуществующие employee_id
+
 create table employees_salary (
 	id serial primary key,
-	employee_id int not null,
-	salary_id int not null, 
-	foreign key (employee_id)
-		references employees (id),
-	foreign key (salary_id)
-		references salary (id)
+	employee_id int not null unique,
+	salary_id int not null 
 );
-
-select * from employees_salary;
 insert into employees_salary (id, employee_id, salary_id)
 values (default, 33, 7 )
 ;
 
+select * from employees_salary;
+
+
 
 --====================================
+--Создать таблицу roles
+--- id. Serial  primary key,
+--- role_name. int, not null, unique
+--Поменять тип столба role_name с int на varchar(30)
+--Наполнить таблицу roles 20 строками:
 
 create table roles (
 id serial primary key, 
@@ -53,9 +88,15 @@ values (default, 'Senior Automation QA engineer')
 select * from roles;
 
 --===================================
+--Создать таблицу roles_employee
+--- id. Serial  primary key,
+--- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+--- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+--Наполнить таблицу roles_employee 40 строками:
+
 create table roles_employee (
 	id serial primary key, 
-	employee_id int not null, 
+	employee_id int not null unique, 
 	role_id int not null, 
 		foreign key (employee_id)
 			references employees (id),
@@ -63,6 +104,8 @@ create table roles_employee (
 			references roles (id)
 );
 
-select * from roles_employee;
+
 insert into roles_employee (id, employee_id, role_id)
 values (default, 6, 7)
+
+select * from roles_employee;
